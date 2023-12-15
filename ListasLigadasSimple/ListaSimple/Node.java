ab30f0117af618237bb31bd9f457a8f6b1d64bec
Al3xtor087 <149035093+Al3xtor087@users.noreply.github.com>
2023-12-15 00:16:45 -0500

    Node.java

--- ListasLigadasSimple/ListaSimple/Node.java
+++ ListasLigadasSimple/ListaSimple/Node.java
@@ -1,6 +1,9 @@
-//clase autorreferenciada
-
-public class Node {
-	String name; //campo de datos
-    Node next;   //campo enlace
+/**
+ * Pérez Talavera Alejandro de Jesús
+ * Clase para crear el enlace a los nodos y la variable clave
+ */
+public class Node
+{
+    String name; // Variable que va a guardar los valores de los nodos
+    Node next; // Se crea el enlace para los nodos
 }
