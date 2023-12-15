103d02ef5c4a14d8263b3bdf1a4ddb319c66d7e9
Al3xtor087 <149035093+Al3xtor087@users.noreply.github.com>
2023-12-15 00:21:01 -0500

    Delete ListasLigadasSimple/ListaSimple directory

--- ListasLigadasSimple/ListaSimple/Node.java
+++
@@ -1,9 +0,0 @@
-/**
- * Pérez Talavera Alejandro de Jesús
- * Clase para crear el enlace a los nodos y la variable clave
- */
-public class Node
-{
-    String name; // Variable que va a guardar los valores de los nodos
-    Node next; // Se crea el enlace para los nodos
-}
